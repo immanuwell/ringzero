f170719abf881b187db273b364aff78334d19a2a
immanuwell <pchpr.00@list.ru>
2026-09-15 20:39:03 +0400

    build: scaffold zig build configuration for the control plane binary

---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "ringzero",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+    });
+    exe.root_module.addIncludePath(b.path("bpf"));
+    exe.root_module.linkSystemLibrary("bpf", .{});
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
+    const run_step = b.step("run", "Run ringzero");
+    run_step.dependOn(&run_cmd.step);
+
+    const mod_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_mod_tests.step);
+}
